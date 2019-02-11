c5fc71854cd3d57f802751a3b1abbe6e633a125c
Suman C <7suman10@gmail.com>
2019-02-11 08:15:32 +0530

    'second'

--- my-app/src/components/SignUp/SignUp.js
+++ my-app/src/components/SignUp/SignUp.js
@@ -5,14 +5,31 @@ class SingUp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            uname:'',
+            password:''
         };
         this.LoginPage=this.LoginPage.bind(this)
-
+        this.handleChange=this.handleChange.bind(this)
+        this.signUp=this.signUp.bind(this)
     }
+
+    handleChange(event) {
+       // alert("hi")
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+    }
+    
     LoginPage(){
         alert("hello")
         alert(this.props.login)
         console.log(this.props)
+    }
+
+    signUp(){
+        alert(this.state.uname);
+        alert(this.state.password)
+        console.log(this.state.uname)
     }
 
 
@@ -29,15 +46,23 @@ class SingUp extends React.Component {
                             <div class="input-group-prepend">
                                 <span class="input-group-text">Username</span>
                             </div>
-                            <input type="text" class="form-control" placeholder="Username" />
+                            <input type="text" class="form-control" 
+                                    placeholder="Username"
+                                    name='uname'
+                                    value={this.state.uname}
+									onChange={this.handleChange} />
                         </div>
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text">Password</span>
                             </div>
-                            <input type="text" class="form-control" placeholder="Password" />
+                            <input type="text" class="form-control" 
+                                    placeholder="Password"
+                                    name='password'
+                                    value={this.state.password}
+									onChange={this.handleChange} />
                         </div>
-                        <button type="button" class="btn loginBtn btn-block">SingUp</button>
+                        <button type="button" class="btn loginBtn btn-block" onClick={this.signUp}>SingUp</button>
                     </form>
                 </div>
                 <div class="modal-footer">
